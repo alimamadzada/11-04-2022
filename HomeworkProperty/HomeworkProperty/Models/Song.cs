0998384e68185d66019acecb2e91054fe53ca2d1
alimamadzada <7b5n43f@code.edu.az>
2022-03-29 00:10:09 +0400

    added 29-03-2022 Tasks.

---
+++ HomeworkProperty/HomeworkProperty/Models/Song.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeworkProperty.Models
+{
+    class Song : Singer
+    {
+        private string _genre;
+        private Singer _singer;
+        public string Genre
+        {
+            get
+            {
+                return _genre;
+            }
+            set
+            {
+                if (value.Contains("Pop") || value.Contains("Rock") || value.Contains("Jazz") || value.Contains("Tehcno"))
+                    _genre = value;
+                else Console.WriteLine("Listde qeyd etdiyiniz Janrda musiqi yoxdur.");
+            }
+        }
+        private int[] Ratings = new int[0];
+        public void AddRating(int rating)
+        {
+            if (rating >= 0 && rating <= 10)
+            {
+                Array.Resize(ref Ratings, Ratings.Length + 1);
+                Ratings[Ratings.Length - 1] = rating;
+            }
+        }
+        public double GetAverageRating()
+        {
+            return Ratings.Average();
+        }
+    }
+}
---
+++ HomeworkProperty/HomeworkProperty/Program.cs
@@ -0,0 +1,17 @@
+﻿using HomeworkProperty.Models;
+using System;
+namespace HomeworkProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song();
+            song.AddRating(2);
+            song.AddRating(6);
+            song.AddRating(4);
+            song.AddRating(14);
+            Console.WriteLine(song.GetAverageRating());
+        }
+    }
+}
