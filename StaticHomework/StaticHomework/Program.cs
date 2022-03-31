23b0bae9f1cd07a4c10936d0cffbcd7e51ad559a
alimamadzada <7b5n43f@code.edu.az>
2022-03-31 20:24:43 +0400

    added 01-04-2022 homework

---
+++ StaticHomework/StaticHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿using StaticHomework.Models;
+using System;
+
+namespace StaticHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Calculator.Multiply(2, 1);
+            Console.WriteLine(num.Power(6)); 
+        }
+    }
+}
