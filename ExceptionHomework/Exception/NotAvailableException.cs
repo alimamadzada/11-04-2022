d4f9972d0f0e362ed68ca234103434a3aa670183
alimamadzada <7b5n43f@code.edu.az>
2022-04-04 02:12:24 +0400

    added 04.04.2022 Homework.

---
+++ ExceptionHomework/Exception/NotAvailableException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHomework
+{
+    class NotAvailableException : Exception
+    {
+        public NotAvailableException() : base()
+        {
+            Console.WriteLine("not available any Exception");
+        }
+        public NotAvailableException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ExceptionHomework/Exception/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHomework
+{
+    class NotFoundException : Exception
+    {
+        public NotFoundException() : base()
+        {
+            Console.WriteLine("not found any Exception");
+        }
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
