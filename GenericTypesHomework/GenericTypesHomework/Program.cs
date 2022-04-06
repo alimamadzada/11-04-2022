1723829d257e310a6ba7af95397905b811ebefde
alimamadzada <7b5n43f@code.edu.az>
2022-04-07 02:36:41 +0400

    added 0[200~7~

---
+++ GenericTypesHomework/GenericTypesHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace GenericTypesHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> ml = new MyList<string>();
+            ml.AddItems("Salam", "Necesen", "ne", "var", "ala", "heyva");
+            //ml.RemoveLast();
+            //ml.Reverse();
+            ml.LastIndexOf();
+            //ml.Print();
+        }
+    }
+}
