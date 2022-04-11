170c48af4448613fa7323308284dded862e2b2a0
alimamadzada <7b5n43f@code.edu.az>
2022-04-11 04:00:27 +0400

    added 11 april's homeworks.

---
+++ JsonHomework/JsonHomework/Program.cs
@@ -0,0 +1,25 @@
+﻿using JsonHomework.Files;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace JsonHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string jsonResp = client.GetStringAsync(@"https://jsonplaceholder.typicode.com/posts").Result;
+
+            List<Posts> posts = JsonConvert.DeserializeObject<List<Posts>>(jsonResp);
+
+            foreach (var item in posts)
+            {
+                Console.WriteLine(item.title);
+            }
+        }
+    }
+}
---
+++ XmlSerializeHomework/XmlSerializeHomework/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Xml.Serialization;
+using Xml2CSharp;
+
+namespace XmlSerializeHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string Valuta = client.GetStringAsync(@"https://www.cbar.az/currencies/08.04.2022.xml").Result;
+            StringReader stringReader = new StringReader(Valuta);
+            XmlSerializer xml = new XmlSerializer(typeof(ValCurs));
+
+            ValCurs valCurs = (ValCurs)xml.Deserialize(stringReader);
+
+            string CurrencyAsJson = JsonConvert.SerializeObject(valCurs);
+            StreamWriter sw = new StreamWriter(@"C:\Users\Asus\source\repos\XmlSerializeHomework\XmlSerializeHomework\Files\xml.json");
+            sw.Write(CurrencyAsJson);
+            sw.Close();
+
+            Console.WriteLine("Zamani qeyd edin: ");
+            string date = Console.ReadLine();
+
+            if (date == valCurs.Date)
+            {
+                StreamWriter sw1 = new StreamWriter(@"C:\Users\Asus\source\repos\XmlSerializeHomework\XmlSerializeHomework\Files\Date.json");
+                sw1.Write(CurrencyAsJson);
+            }
+
+        }
+    }
+}
